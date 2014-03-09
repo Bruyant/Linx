37dea895644c4ea2b543c866784b4919601353d1
Nicolas <nicolas@new-host-2.home>
2014-03-09 03:50:19 +0100

    created test suite for reffrequency

--- LibLinx_test.py
+++ LibLinx_test.py
@@ -6,30 +6,36 @@ Created on Sat Mar  8 21:58:26 2014
 """
 
 import unittest
+import testtools
 from numpy import arange,linspace,zeros,sin,pi
 
-def GenerateRefSignal(FreqRef):
-    sample_rate=500E3
-    NSamples=1000000
+from LibLinx import FindRefFreq
+
+def GenerateRefSignal(FreqRef=12345,sample_rate=500E3,NSamples=1000000):
     data = zeros((NSamples,5))
     data[:,0] = arange(0,NSamples)/sample_rate
     data[:,1]=sin(data[:,0]*FreqRef*2*pi)
     return data
 
-class SimpleWidgetTestCase(unittest.TestCase):
+class TestFindRefFreq(unittest.TestCase):
     def setUp(self):
         # base setup code
-        self.n=1
+        self.data=GenerateRefSignal()
 
-class DefaultWidgetSizeTestCase(SimpleWidgetTestCase):
+class TestFindRefFreqSingle(TestFindRefFreq):
     def runTest(self):
         #first test
-        self.assertEqual(self.n,1)
+        res=FindRefFreq(self.data[:,1],500E3)
+        self.assertTrue(res-12345<0.01)
     
-class WidgetResizeTestCase(SimpleWidgetTestCase):
+class TestFindRefFreq2Freq(TestFindRefFreq):
     def runTest(self):
-        #second test
-        self.assertEqual(self.n,2)
+        #mutiply by 2 the ref frequency
+        self.data[:,1]=sin(self.data[:,0]*2*12345*2*pi)
+        res=FindRefFreq(self.data[:,1],500E3)
+        self.assertTrue(res-12345*2<0.01)
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFindRefFreq)
+    concurrent_suite = testtools.ConcurrentStreamTestSuite(lambda: ((case, None) for case in suite))
+    concurrent_suite.run(testtools.StreamResult())
