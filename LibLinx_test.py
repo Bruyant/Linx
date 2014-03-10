386a3905320b83711854bbf92bc3daff8026e31d
Nicolas <nicolas@new-host-2.home>
2014-03-10 23:06:25 +0100

    add a test and some typos

--- LibLinx_test.py
+++ LibLinx_test.py
@@ -6,30 +6,36 @@ Created on Sat Mar  8 21:58:26 2014
 """
 
 import unittest
+import logging
 
+import random
+from numpy import arange, linspace, zeros, sin, pi, max
 from LibLinx import FindRefFreq
 
 _multiprocess_can_split_ = True
 
-tolerance=0.001 # 0.01% -> i.e; 1e-4
+tolerance = 0.001   # 0.01% -> i.e; 1e-4
 
-def GenerateRefSignal(FreqRef=54321,sample_rate=500E3,NSamples=1000000):
-    data = zeros((NSamples,5))
-    data[:,0] = arange(0,NSamples)/sample_rate
-    data[:,1]=sin(data[:,0]*FreqRef*2*pi)
+
+def GenerateRefSignal(FreqRef=54321, sample_rate=500E3, NSamples=1000000):
+    data = zeros((NSamples, 5))
+    data[:, 0] = arange(0, NSamples)/sample_rate
+    data[:, 1] = sin(data[:, 0]*FreqRef*2*pi)
     return data
-    
-def ComputeError(FreqRef,Res):
-    return abs((Res-FreqRef))/FreqRef*100
+
+
+def ComputeError(FreqRef, Res):
+    return abs(max(Res-FreqRef))/max(FreqRef)*100
+
 
 class TestFindRefFreq(unittest.TestCase):
     def setUp(self):
         # base setup code
-        
-        self.FreqRef=54321,
-        self.sample_rate=500E3
-        self.NSamples=1000000
-        self.data=GenerateRefSignal(self.FreqRef,
+
+        self.FreqRef = 54321
+        self.sample_rate = 500E3
+        self.NSamples = 1000000
+        self.data = GenerateRefSignal(self.FreqRef,
                                     self.sample_rate,self.NSamples)
         logging.debug('FreqRef={0},sample_rate={1},NSamples={2}'.format(
                         self.FreqRef,self.sample_rate,self.NSamples))                         
@@ -55,10 +61,26 @@ class TestFindRefFreqSamplerateRandom(TestFindRefFreq):
         self.data=GenerateRefSignal(54321,self.sample_rate)
         self.res=FindRefFreq(self.data[:,1],self.sample_rate)
         print self.res,self.sample_rate
-        self.assertTrue(ComputeError(54321,self.res)<tolerance)       
+        self.assertTrue(ComputeError(54321,self.res)<tolerance)
+
+
+class TestFindRefFreqLinspaceFreq(TestFindRefFreq):
+    def runTest(self):
+        self.sample_rate = 500E3
+        self.data = GenerateRefSignal(54321, self.sample_rate)
+        # define frequency to test
+        FreqsTest = linspace(1000, 20000, num=5)
+        res = zeros(len(FreqsTest))
+
+        for index, FreqRef in enumerate(FreqsTest):
+            self.data[:, 1] = sin(self.data[:, 0]*FreqRef*2*pi)
+            res[index] = FindRefFreq(self.data[:, 1], self.sample_rate)
+
+        self.assertTrue(ComputeError(FreqsTest, res) < tolerance)
+
 
 class test_many_SamplesRates(TestFindRefFreq):
-    def runTest(self):      
+    def runTest(self):   
         for _ in range(200):
             TestFindRefFreqSamplerateRandom()
 
@@ -69,4 +91,6 @@ class test_many_Freqs(TestFindRefFreq):
             TestFindRefFreqSingleRandom()
 
 if __name__ == '__main__':
-    unittest.main()    
+    #configure logging
+    logging.basicConfig(filename='LibLinx_test.log', level=logging.DEBUG)
+    unittest.main()
