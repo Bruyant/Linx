fc7017e1f448ef1d4db6bd5f504f73ecf8f0c3de
Nicolas <nicolas@new-host-2.home>
2014-03-10 22:39:50 +0100

    Setup nose test

--- LibLinx_test.py
+++ LibLinx_test.py
@@ -6,36 +6,67 @@ Created on Sat Mar  8 21:58:26 2014
 """
 
 import unittest
-import testtools
-from numpy import arange,linspace,zeros,sin,pi
 
 from LibLinx import FindRefFreq
 
-def GenerateRefSignal(FreqRef=12345,sample_rate=500E3,NSamples=1000000):
+_multiprocess_can_split_ = True
+
+tolerance=0.001 # 0.01% -> i.e; 1e-4
+
+def GenerateRefSignal(FreqRef=54321,sample_rate=500E3,NSamples=1000000):
     data = zeros((NSamples,5))
     data[:,0] = arange(0,NSamples)/sample_rate
     data[:,1]=sin(data[:,0]*FreqRef*2*pi)
     return data
+    
+def ComputeError(FreqRef,Res):
+    return abs((Res-FreqRef))/FreqRef*100
 
 class TestFindRefFreq(unittest.TestCase):
     def setUp(self):
         # base setup code
-        self.data=GenerateRefSignal()
+        
+        self.FreqRef=54321,
+        self.sample_rate=500E3
+        self.NSamples=1000000
+        self.data=GenerateRefSignal(self.FreqRef,
+                                    self.sample_rate,self.NSamples)
+        logging.debug('FreqRef={0},sample_rate={1},NSamples={2}'.format(
+                        self.FreqRef,self.sample_rate,self.NSamples))                         
 
-class TestFindRefFreqSingle(TestFindRefFreq):
+
+class TestFindRefFreqBasic(TestFindRefFreq):
     def runTest(self):
-        #first test
         res=FindRefFreq(self.data[:,1],500E3)
-        self.assertTrue(res-12345<0.01)
-    
-class TestFindRefFreq2Freq(TestFindRefFreq):
+        self.assertTrue(ComputeError(54321,res)<tolerance) 
+
+class TestFindRefFreqSingleRandom(TestFindRefFreq):
     def runTest(self):
-        #mutiply by 2 the ref frequency
-        self.data[:,1]=sin(self.data[:,0]*2*12345*2*pi)
+        #Single random test around 50KHz
+        self.FreqRef=40E3+random.randint(0,20000)
+        self.data[:,1]=sin(self.data[:,0]*self.FreqRef*2*pi)
         res=FindRefFreq(self.data[:,1],500E3)
-        self.assertTrue(res-12345*2<0.01)
+        self.assertTrue(ComputeError(self.FreqRef,res)<tolerance)
+        
+class TestFindRefFreqSamplerateRandom(TestFindRefFreq):
+    def runTest(self):
+        #random samplerate
+        self.sample_rate=200E3+random.randint(0,3E5)
+        self.data=GenerateRefSignal(54321,self.sample_rate)
+        self.res=FindRefFreq(self.data[:,1],self.sample_rate)
+        print self.res,self.sample_rate
+        self.assertTrue(ComputeError(54321,self.res)<tolerance)       
+
+class test_many_SamplesRates(TestFindRefFreq):
+    def runTest(self):      
+        for _ in range(200):
+            TestFindRefFreqSamplerateRandom()
+
+
+class test_many_Freqs(TestFindRefFreq):
+    def runTest(self):
+        for _ in range(200):
+            TestFindRefFreqSingleRandom()
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestFindRefFreq)
-    concurrent_suite = testtools.ConcurrentStreamTestSuite(lambda: ((case, None) for case in suite))
-    concurrent_suite.run(testtools.StreamResult())
+    unittest.main()    
